134a71d4a7e40d747710c559a713cd7e3da20085
Brian Vu <brian.vu12@gmail.com>
2023-09-16 21:51:26 -0400

    fix: reset the whole database

--- next-app/prisma/migrations/20230917015007_reset_everything/migration.sql
+++ next-app/prisma/migrations/20230917015007_reset_everything/migration.sql
@@ -1,20 +1,20 @@
 -- CreateTable
 CREATE TABLE "conversations" (
-    "conversation_id" INT8 NOT NULL DEFAULT unique_rowid(),
+    "conversation_id" STRING NOT NULL,
     "timestamp" TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP,
-    "participants" INT8[],
-    "transcription" STRING,
+    "text" STRING,
+    "summary" STRING,
+    "generated_prompts" STRING,
 
     CONSTRAINT "conversations_pkey" PRIMARY KEY ("conversation_id")
 );
 
 -- CreateTable
 CREATE TABLE "users" (
-    "user_id" INT8 NOT NULL DEFAULT unique_rowid(),
+    "user_id" STRING NOT NULL,
     "name" STRING(100),
     "image_url" STRING(255),
     "short_bio" STRING,
 
     CONSTRAINT "users_pkey" PRIMARY KEY ("user_id")
 );
-
