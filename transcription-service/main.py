04dc46e13c6b68c9a42611eed767c419c9648bf3
Ahmed-Labs <ahmedelhagjr@gmail.com>
2023-09-16 22:04:49 -0400

    add mp3 audio processing endpoint

--- transcription-service/main.py
+++ transcription-service/main.py
@@ -3,24 +3,34 @@ from pathlib import Path
 from fastapi import FastAPI, UploadFile
 from fastapi.responses import StreamingResponse
 from dotenv import load_dotenv
+from fastapi.middleware.cors import CORSMiddleware
+import assemblyai as aai
+import subprocess
 import openai
 import os
 import cohere, httpx
 import uvicorn, asyncio
-
 from services.services import generate_conversation_prompts, generate_conversation_summary
 
 load_dotenv()
 
-
 app = FastAPI()
 tmp_file_dir = "/tmp"
 Path(tmp_file_dir).mkdir(parents=True, exist_ok=True)
-openai.api_key = ""
 
+aai.settings.api_key = os.getenv("AAI_API_KEY")
+openai.api_key = os.getenv("OPENAI_API_KEY")
 COHERE_API_KEY = os.getenv("COHERE_API_KEY")
-
 co = cohere.Client(COHERE_API_KEY)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Change this to the origin(s) of your frontend
+    allow_credentials=True,
+    allow_methods=[""],  # Change this to the allowed HTTP methods
+    allow_headers=["*"],  # Change this to the allowed headers
+)
+
 @app.get("/process")
 async def process(WEB_URL:str="http://localhost:3000/api/webhooks"):
     await asyncio.sleep(5)
@@ -72,6 +82,47 @@ async def post_media_file(file: UploadFile):
 
         assert response != ""
         return response
+
+@app.post("/api/audio")
+async def process_audio(file: UploadFile):
+    with open(os.path.join(tmp_file_dir, file.filename), 'wb') as disk_file:
+        print(os.path.join(tmp_file_dir, file.filename))
+        input_file = await file.read()
+
+        disk_file.write(input_file)
+       
+        os.listdir()
+        print(f"Received file named {file.filename} containing {len(input_file)} bytes. ")
+        print(f"{tmp_file_dir}/{file.filename}")
+        print(os.listdir())
+        subprocess.run(f'ffmpeg -i "{os.path.join(tmp_file_dir, file.filename)}" "out".mp3',shell=True)
+        
+        audio_file= open("out.mp3", "rb")
+        # transcription = openai.Audio.transcribe("whisper-1", audio_file)
+        config = aai.TranscriptionConfig(speaker_labels=True, speakers_expected=2)
+
+        transcriber = aai.Transcriber(config=config)
+        transcript = transcriber.transcribe("out.mp3")
+        os.remove("out.mp3")
+        for utterance in transcript.utterances:
+            speaker = utterance.speaker
+            text = utterance.text
+            print(f"Speaker {speaker}: {text}")
+        co = cohere.Client(COHERE_API_KEY)
+
+        script_text = transcript.text
+        if len(script_text) < 250: 
+            script_text += "".join(" " for i in range(255-len(script_text)))
+
+        response = co.summarize(text=script_text, length='medium',
+            format='paragraph',
+            model='summarize-xlarge',
+            additional_command='Seperate into Bullet Points',
+            temperature=0.3,
+        ).summary
+        return {"Summary": response}
+
+    return {"Hello": file.filename}
 
 
 def main():
