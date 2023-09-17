ed9d2e5752a0e79787820e7be0c54233bc3068d4
Brian Vu <brian.vu12@gmail.com>
2023-09-17 00:29:50 -0400

    feat: webhook now updates the correct conversation

--- next-app/src/app/api/webhooks/route.ts
+++ next-app/src/app/api/webhooks/route.ts
@@ -1,19 +1,25 @@
 import { NextResponse, NextRequest } from "next/server";
-import { createConversation } from "@/lib/actions/conversations.actions";
+import {
+  createConversation,
+  updateConversation,
+} from "@/lib/actions/conversations.actions";
 
 export interface AudioAnalysis {
   text: string;
   summary: string;
   prompt: string;
+  conversation_id: string;
 }
 
 export async function POST(request: NextRequest) {
   // Our Python backends sends webhook containing a JSON body with the audio analysis fields after it has finished processing the audio
   const body: AudioAnalysis = await request.json();
-  const conversation = await createConversation(
+  console.log("body", body);
+  const conversation = await updateConversation(
     body.text,
     body.summary,
-    body.prompt
+    body.prompt,
+    body.conversation_id
   );
   return await NextResponse.json({ "conversation": conversation });
 }
--- next-app/src/lib/actions/conversations.actions.ts
+++ next-app/src/lib/actions/conversations.actions.ts
@@ -20,3 +20,27 @@ export const createConversation = async (
     console.error("Error creating conversation:", error);
   }
 };
+
+export const updateConversation = async (
+  text: string,
+  summary: string,
+  generated_prompts: string,
+  conversation_id: string
+) => {
+  try {
+    const updatedConversation = await prisma.conversations.update({
+      where: {
+        conversation_id: conversation_id,
+      },
+      data: {
+        text: text,
+        summary: summary,
+        generated_prompts: generated_prompts,
+      },
+    });
+    console.log(updatedConversation);
+    return updatedConversation;
+  } catch (error) {
+    console.error("Error updating conversation: ", error);
+  }
+};
